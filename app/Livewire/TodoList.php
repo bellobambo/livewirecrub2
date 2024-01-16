7f2c11daf9138f173f4a91063158b4744224afda
bellobambo <bellobambo21@gmail.com>
2024-01-16 11:00:08 +0100

    final livewire crud

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Todo;
+use Exception;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -16,6 +17,12 @@ class TodoList extends Component
     public $name;
     public $search;
 
+    public $editingTodoID;
+
+    #[Rule('required|min:3|max:50')]
+
+    public $editingTodoName;
+
     public function create()
     {
         $validated = $this->validateOnly('name');
@@ -25,18 +32,53 @@ class TodoList extends Component
         $this->reset('name');
 
         session()->flash('success', 'Todo Created');
+        $this->resetPage();
     }
 
-    public function delete($todoID){
-       Todo::find($todoID)->delete();
+    public function delete($todoID)
+    {
+        try {
+            Todo::findOrFail($todoID)->delete();
+
+        } catch (Exception $e) {
+            session()->flash('error', 'Failed to delete');
+        }
+    }
+
+
+    public function toggle($todoID)
+    {
+        $todo = Todo::find($todoID);
+        $todo->completed = !$todo->completed;
+        $todo->save();
+    }
+    public function edit($todoID)
+    {
+        $this->editingTodoID = $todoID;
+        $this->editingTodoName = Todo::find($todoID)->name;
+    }
+    public function cancelEdit()
+    {
+        $this->reset('editingTodoID', 'editingTodoName');
+    }
+    public function update()
+    {
+        $this->validateOnly('editingTodoName');
+        Todo::find($this->editingTodoID)->update(
+            [
+                'name' => $this->editingTodoName
+            ]
+        );
+
+        $this->cancelEdit();
     }
 
     public function render()
     {
 
 
-        return view('livewire.todo-list' , [
-            'todos' => Todo::latest()->where('name' , 'like' , "%{$this->search}%")->paginate(5)
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(5)
         ]);
     }
 }
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,4 +1,15 @@
 <div>
+    @if (session('error'))
+    <div role="alert">
+        <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+          Something Went Wrong
+        </div>
+        <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+          <p>Something not ideal might be happening.</p>
+          <p>{{session('error')}}</p>
+        </div>
+      </div>
+    @endif
     @include('livewire.includes.create-todo-box')
 
     @include('livewire.includes.search-box')
